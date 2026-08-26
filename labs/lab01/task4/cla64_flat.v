72be4c7e74f784dcfda852c83843a6c34b28b296
Nirbhay S Matharu <nirbhaymatharu108@gmail.com>
2026-08-26 10:46:02 +0000

    lab01 task4

--- labs/lab01/task4/cla64_flat.v
+++ labs/lab01/task4/cla64_flat.v
@@ -27,8 +27,8 @@ module cla64_flat(
   genvar i;
   generate
     for (i = 0; i < 64; i = i + 1) begin : gen_pg
-      xor (p[i], a[i], b[i]);
-      and (g[i], a[i], b[i]);
+      assign p[i] = a[i] ^ b[i];
+      assign g[i] = a[i] & b[i];
     end
   endgenerate
 
