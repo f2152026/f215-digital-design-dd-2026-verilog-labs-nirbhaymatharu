ecd36228bfc31757faf75ed8abb60afec3cd3f72
Nirbhay S Matharu <nirbhaymatharu108@gmail.com>
2026-08-26 06:24:33 +0000

    lab01 task2

--- labs/lab01/task2/FA_Gate.v
+++ labs/lab01/task2/FA_Gate.v
@@ -29,7 +29,7 @@ module FA_Gate(
   xor #(2,3) (ps,  a,   b);
   and #(2,3) (pc1, a,   b);
   xor #(2,3) (sum, cin, ps);
-  and #(2,4) (pc2, cin, ps);
-  or #(2,4) (cout, pc1, pc2);
+  and #(2,3) (pc2, cin, ps);
+  or #(2,3) (cout, pc1, pc2);
 
 endmodule
